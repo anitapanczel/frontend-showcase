f3230ce383a8bc8f4899f8aa42e8ddb90309c3c6
anitapanczel <anita@panczel.hu>
2021-10-14 11:36:07 +0200

    updated

---
+++ script.js
@@ -0,0 +1,14 @@
+function _load() {
+    console.log("Az oldal betöltődött!");
+    document.getElementById("root").classList.add("loaded");
+    
+        function rootClicked(){
+            console.log("A root ID div-re  kattintottunk.");
+        }
+        
+        
+        document.getElementById("root").addEventListener("click", rootClicked);
+    }
+    
+    
+window.addEventListener("load", _load);
